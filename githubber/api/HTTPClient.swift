e712463dd81e1c56dec9993de1f35ca6d9fae5a1
Piotr Żużel <zuzelpiotr@gmail.com>
2016-10-27 20:11:54 +0200

    Add HTTP client stub

---
+++ githubber/api/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClient.swift
+//  githubber
+//
+//  Created by Piotr Żużel on 27/10/16.
+//  Copyright © 2016 Piotr Żużel. All rights reserved.
+//
+
+import Foundation
+
+class HTTPClient{
+    enum Method: String {
+        case GET, POST, PUT, DELETE
+    }
+}
