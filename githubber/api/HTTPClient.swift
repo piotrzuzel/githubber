32c1a237369d417aecbb782df69623385839b88e
Piotr Żużel <zuzelpiotr@gmail.com>
2016-10-27 21:31:06 +0200

    Add generic HTTPClient request method

--- githubber/api/HTTPClient.swift
+++ githubber/api/HTTPClient.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+extension Dictionary where Key: ExpressibleByStringLiteral, Value:Any {
+    var urlKeyValue: String {
+        get{
+            return (self.flatMap({ (key, value) -> String in
+                return "\(key)=\(value)"
+            }) as Array).joined(separator: "&")
+        }
+    }
+}
+
 class HTTPClient{
     enum Method: String {
         case GET, POST, PUT, DELETE
@@ -18,4 +28,39 @@ class HTTPClient{
     var timeoutInterval: TimeInterval = 10.0
     
     private var task: URLSessionDataTask?
+    
+    func request(url: URL,
+                 method: Method,
+                 parameters: [String: Any]? = nil,
+                 parametersInUrl: Bool = false,
+                 headers: [String: String]? = nil,
+                 success: ((Data?) -> Void)? = nil,
+                 failure: ((_ data: Data?, _ error: NSError, _ responseCode: Int) -> Void)? = nil){
+        
+        var mutableRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: self.timeoutInterval)
+        mutableRequest.allHTTPHeaderFields = headers
+        mutableRequest.httpMethod = method.rawValue
+        
+        if (parameters != nil) {
+            if parametersInUrl {
+                let paramsString = parameters?.urlKeyValue
+                mutableRequest.url = mutableRequest.url?.appendingPathComponent("?"+paramsString!)
+            } else {
+                mutableRequest.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+            }
+        }
+        
+        
+        let session = URLSession.shared
+        task = session.dataTask(with: mutableRequest, completionHandler: { (data, response, error) in
+            success?(data)
+        })
+        
+        task?.resume()
+        
+    }
+    
+    func cancel() {
+        task?.cancel()
+    }
 }
